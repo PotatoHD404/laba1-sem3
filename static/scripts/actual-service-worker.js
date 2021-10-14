67e857a92af5a5323189601e18b6f2f5f590e27a
PotatoHD404 <31506993+PotatoHD404@users.noreply.github.com>
2021-10-14 13:52:01 +0300

    Added service worker contents for presentation

---
+++ static/scripts/actual-service-worker.js
@@ -0,0 +1,30 @@
+
+self.importScripts('/scripts/Laba3.js');
+
+function cin(input) {
+  if (self.instance?.listening) {
+    // const event = new CustomEvent('cin', { input: input });
+    self.instance.listening = false;
+    self.instance.message(input);
+    // self.instance.dispatchEvent(event);
+  } else {
+    console.log('Not responding to your input. Try again later.');
+  }
+}
+
+self.onmessage = async (e) => {
+
+  if (e.data === 'init') {
+
+    self.instance = await Module({
+      'print': (data) => {
+        postMessage(data);
+      }
+    });
+
+    self.instance.listening = false;
+    self.instance.start();
+  } else {
+    cin(e.data);
+  }
+};
